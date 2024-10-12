2d3a05eca4e16dfb92452bc387eb9a1b3fd1004f
Spec-DY <ibonehp@gmail.com>
2024-10-11 23:55:27 -0700

    largely improve toggle theme button

--- Screens/Settings.js
+++ Screens/Settings.js
@@ -1,14 +1,23 @@
 import React, { useContext } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Switch } from 'react-native';
 import { AppContext } from '../context/AppContext';
 
 const Settings = () => {
   const { theme, toggleTheme, themeStyles } = useContext(AppContext);
 
+  const isDarkMod = theme === 'dark'
+
   return (
     <View style={[styles.container, { backgroundColor: themeStyles[theme].backgroundColor }]}>
-      <Text style={{ color: themeStyles[theme].textColor }}>Current Theme: {theme}</Text>
-      <Button title="Toggle Theme" onPress={toggleTheme} />
+      <Switch
+            trackColor={{ false: "#767577", true: "#81b0ff" }}
+            thumbColor={isDarkMod ? "#f5dd4b" : "#f4f3f4"}
+            ios_backgroundColor="#3e3e3e"
+            // toogleTheme callback
+            onValueChange={toggleTheme}
+            value={isDarkMod}
+            style={{transform: [{ scaleX: 5 }, { scaleY: 5 }]}}
+      />
     </View>
   );
 };
